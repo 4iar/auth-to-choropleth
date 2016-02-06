d9ff30607dd7c2633299aacc364d11b0359bfa94
4iar <4iar@users.noreply.github.com>
2016-02-06 17:00:32 +0000

    Check for PAM login failure instead of checking failed pw/invalid user/etc separately

--- log_parsers.py
+++ log_parsers.py
@@ -13,9 +13,7 @@ class SSHDLogParser:
         fp = open(path, 'r')
         log_text = fp.read()
 
-        ip_addresses.append(findall("(?<=Failed password for)(?: \w+ from )(.+)(?= port)", log_text))
-        ip_addresses.append(findall("(?<=Invalid user )(?:\w+ from )(.+)", log_text))
-        ip_addresses.append(findall("(?<=Did not receive identification string from )(.+)", log_text))
+        ip_addresses.append(findall("(?:sshd\[[0-9]+\]:.+authentication failure.+)(?<=rhost=)(\S+)", log_text))
 
         return [ip for ip_matches in ip_addresses for ip in ip_matches]
 
